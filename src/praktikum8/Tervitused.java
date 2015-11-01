6381588887a4e950acfd4b1102aeb526d3151b68
eriks6lg <eriks1995@gmail.com>
2015-11-01 13:57:39 +0200

    Praktikum 8 - Tervitused.java

--- src/praktikum8/Tervitused.java
+++ src/praktikum8/Tervitused.java
@@ -1,4 +1,5 @@
 package praktikum8;
+import java.util.ArrayList;
 import lib.TextIO;
 
 public class Tervitused {
@@ -7,7 +8,28 @@ public class Tervitused {
 		
 		ArrayList<Inimene> inimesed = new ArrayList<Inimene>();
 		
-		//TEST
+		System.out.println("Tere! See programm loob uusi inimesi Teie sisestatud\n"
+				+ "nimede ja vanuste järgi. Kui te ei soovi enam inimesi sisestada,\n"
+				+ "sisestage 0");
+		int i = 0;
+		while(true) {
+			i++;
+			System.out.printf("%d. inimese nimi: ", i);
+			String nimi = TextIO.getlnString();
+			if (nimi.equals("0"))
+				break;
+			System.out.printf("%d. inimese vanus: ", i);
+			int vanus = TextIO.getlnInt();
+			if (vanus == 0)
+				break;
+			inimesed.add(new Inimene(nimi, vanus));
+			System.out.println();
+		}
+		for (Inimene inimene: inimesed) {
+			
+			inimene.tervita();
+		}
+		
 	}
 
 }
